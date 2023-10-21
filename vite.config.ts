3735548f7d3e9787763a0ac61c3935247208d61c
Ulf Sauer <ulf.sauer@gmail.com>
2023-10-21 14:00:38 +0200

    Fix missing base path for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {plugins: [vue()],
+  base: env.BASE_URL || '',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+}
 });
